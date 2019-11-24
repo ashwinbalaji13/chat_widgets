c4ec968221c2cb92cbccd67690994698e6a30115
ashwinbalaji13 <ashwin.achu.1311@gmail.com>
2019-11-24 15:33:17 +0530

    widget done

--- chat_widget_html/js/script.js
+++ chat_widget_html/js/script.js
@@ -1,32 +1,32 @@
 let active = false;
 function openChatWindow() {
-	// if (active) {
-	// 	document.getElementById('chat-window').style.display = 'none';
-	// } else {
-	// 	document.getElementById('chat-window').style.display = 'visible';
-	// }
-	// console.log(active);
+  // if (active) {
+  // 	document.getElementById('chat-window').style.display = 'none';
+  // } else {
+  // 	document.getElementById('chat-window').style.display = 'visible';
+  // }
+  // console.log(active);
 
-	// active = !active;
-	var currentstate = document.getElementById('chat-window').style.visibility;
-	//Make newstate the opposite of the current state.
-	var newstate = currentstate == 'hidden' ? 'visible' : 'hidden';
-	var popupicon = newstate == 'hidden' ? 'visible' : 'hidden';
+  // active = !active;
+  var currentstate = document.getElementById("chat-window").style.visibility;
+  //Make newstate the opposite of the current state.
+  var newstate = currentstate == "hidden" ? "visible" : "hidden";
+  var popupicon = newstate == "hidden" ? "visible" : "hidden";
 
-	//Apply the new state.
-	document.getElementById('chat-window').style.visibility = newstate;
-	console.log('popupicon', popupicon);
+  //Apply the new state.
+  document.getElementById("chat-window").style.visibility = newstate;
+  console.log("popupicon", popupicon);
 
-	document.getElementById('chat_icon').style.visibility = popupicon;
+  document.getElementById("chat_icon").style.visibility = popupicon;
 }
 
 window.onload = function() {
-	//Start with the div visible
-	document.getElementById('chat-window').style.visibility = 'hidden';
-	document.getElementById('chat_icon').style.visibility = 'visible';
+  //Start with the div visible
+  document.getElementById("chat-window").style.visibility = "hidden";
+  document.getElementById("chat_icon").style.visibility = "visible";
 
-	//Listen for click on button.
-	// document.getElementById('chat_icon').addEventListener('click', function() {
-	// 	//Get current state of CSS display property.
-	// });
+  //Listen for click on button.
+  // document.getElementById('chat_icon').addEventListener('click', function() {
+  // 	//Get current state of CSS display property.
+  // });
 };
