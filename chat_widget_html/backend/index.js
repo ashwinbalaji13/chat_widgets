cdb23ad8b456b5ec13714fc7923c23ced540604e
Ashwin Balaji R <ashwinbalaji.r@cognizant.com>
2019-11-25 19:28:48 +0530

    added backend

--- chat_widget_html/backend/index.js
+++ chat_widget_html/backend/index.js
@@ -1,20 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const dialogflow = require("dialogflow");
+const dialogflow = require('dialogflow');
 
-const connecter = require("./dialogflow-connecter");
+const connecter = require('./dialogflow-connecter');
 app.use(cors());
 // app.use(express.json());
-app.get("/", (req, res) => {
-  console.log("get successful");
+app.get('/', (req, res) => {
+	console.log('get successful');
 
-  res.send({ response: "hello" });
+	res.send({ response: 'hello' });
 });
-app.post("/smalltalk", express.json(), async (req, res) => {
-  console.log(req.body);
-  res.send(await connecter.sendTextMessageToDialogFlow(req.body.query, "1234"));
+app.post('/smalltalk', express.json(), async (req, res) => {
+	console.log(req.body);
+	let response = await connecter.sendTextMessageToDialogFlow(req.body.query, req.body.sessionID);
+	console.log(response);
+
+	res.send({ response });
 });
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+	console.log('listening on port 3000');
 });
